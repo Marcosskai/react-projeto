8f2e1749e1664a3a88be31f148f86ca1497f0553
Marcosskai <marcos.gabrielr150@gmail.com>
2024-04-21 22:10:13 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-    return <h1>Hello World</h1>;
+interface MeuBotaoProps {
+    texto: string
 }
 
-export default App;
+function MeuBotao(props: MeuBotaoProps){
+    return <button className="button">{props.texto}</button>
+}
+
+export function App() {
+    return (
+        <div>
+            <MeuBotao texto= "Clique aqui" />
+            <MeuBotao texto= "Botão 3" />
+            <MeuBotao texto= "Botão 2" />
+        </div>
+    )
+}
