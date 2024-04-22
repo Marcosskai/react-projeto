bc6d0cd66c3407b4c73d45ac276e3c4453bb1aa5
Marcosskai <marcos.gabrielr150@gmail.com>
2024-04-22 20:48:59 -0300

    Modificações

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -2,12 +2,55 @@ import { UserSearch } from 'lucide-react';
 
 export function AttendeeList() {
     return (
-        <div className="flex gap-3 items-center">
-            <h1 className="text-2xl font-bold">Participantes</h1>
-            <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
-             <UserSearch /> 
-             <input className="bg-transparent flex-1 outline-none" placeholder="Buscar Participante..."/>
+        <div>
+            <div className="flex gap-3 items-center">
+                <h1 className="text-2xl font-bold">Participantes</h1>
+                <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+                    <UserSearch className="size-4 text-emerald-300" />
+                    <input
+                        className="bg-transparent flex-1 outline-none"
+                        placeholder="Buscar Participante..."
+                    />
+                </div>
             </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox" />
+                        </th>
+                        <th>Código</th>
+                        <th>Participantes</th>
+                        <th>Data de Inscrição</th>
+                        <th>Data do check-in</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="checkbox" />
+                        </td>
+                        <td>123456</td>
+                        <td>
+                            <div>
+                                <span>Diego Marcos</span>
+                                <span>diego@gmail.com.com</span>
+                            </div>
+                        </td>
+                        <td>7 dias atrás</td>
+                        <td>3 dias atrás</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={3}>Mostrando 10 de 228 itens</td>
+                        <td colSpan={3}>Página 1 de 23</td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     );
 }
