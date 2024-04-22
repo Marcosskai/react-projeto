c2a12306bf33431895c501a8524d3481a04aa7ce
Marcosskai <marcos.gabrielr150@gmail.com>
2024-04-21 22:41:13 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ interface MeuBotaoProps {
 }
 
 function MeuBotao(props: MeuBotaoProps){
-    return <button className="button">{props.texto}</button>
+    return <button className="bg-orange-400 h-10 px-3 rounded">{props.texto}</button>
 }
 
 export function App() {
     return (
-        <div>
+        <div className="flex gap-2">
             <MeuBotao texto= "Clique aqui" />
             <MeuBotao texto= "Botão 3" />
             <MeuBotao texto= "Botão 2" />
