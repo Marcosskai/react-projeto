236bf7c4d3913ec57e5ca5e10d642a535b8626b0
Marcosskai <marcos.gabrielr150@gmail.com>
2024-04-22 20:29:10 -0300

    commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,11 +4,11 @@ import nlwUniteIcon from "../assets/nlw-unite-icon.svg"
 export function Header() {
    
    return (
-   <div className="flex items-center gap-5">
+   <div className="flex items-center gap-5 py-2">
      <img src={nlwUniteIcon} />
 
      <nav className="flex items-center gap-5">
-      <a href="" className="font-medium text-sm" >Eventos</a>
+      <a href="" className="font-medium text-sm text-zinc-300" >Eventos</a>
       <a href=""className="font-medium text-sm" >Participantes</a>
 
      </nav>
