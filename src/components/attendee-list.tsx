4f60d11b9f8fff8ca53875025bf7dfd98940b2bc
Marcos <marcos.gabrielr150@gmail.com>
2024-04-29 17:06:38 -0300

    commit

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -50,9 +50,8 @@ export function AttendeeList() {
           <tbody>
              {Array.from({ length: 9 }).map((_, i) => {
                     return (
-                        <tr
-                            key={i}
-                            className="border-b border-white/10">
+                        <tr key={i} className="border-b border-white/10 hover: bg-white/5">
+                            
                         <td className="py-3 px-4 text-sm text-zinc-300">
                     <input type="checkbox" className='size-4 bg-black/20 rounded'/>
                        </td>
