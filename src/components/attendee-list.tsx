61a05947e8193e9e3218ee8b9ac82aa8425fcf79
Marcosskai <marcos.gabrielr150@gmail.com>
2024-04-29 20:18:23 -0300

    commit

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -16,102 +16,111 @@ export function AttendeeList() {
                     <UserSearch className="size-4 text-emerald-300" />
                     <input
                         className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm"
-                        placeholder="Buscar Participante..."/>
-         </div>
-          </div>
+                        placeholder="Buscar Participante..."
+                    />
+                </div>
+            </div>
 
-           <div className="border border-white/10 rounded-lg">
-            <table className="w-full">
-                <thead>
-                    <tr className="border-b border-white/10">
-                        <th
-                            style={{ width: 48 }}
-                            className="py-3 px-4 text-sm font-semibold text-left">
-                                <input type="checkbox" className='size-4 bg-black/20 rounded'/>
-                        </th>
-                        < th className="py-3 px-4 text-sm font-semibold text-left">
+            <div className="border border-white/10 rounded-lg">
+                <table className="w-full">
+                    <thead>
+                        <tr className="border-b border-white/10">
+                            <th
+                                style={{ width: 48 }}
+                                className="py-3 px-4 text-sm font-semibold text-left"
+                            >
+                                <input
+                                    type="checkbox"
+                                    className="size-4 bg-black/20 rounded"
+                                />
+                            </th>
+                            <th className="py-3 px-4 text-sm font-semibold text-left">
                                 Código
-                        </th>
-                        < th className="py-3 px-4 text-sm font-semibold text-left">
+                            </th>
+                            <th className="py-3 px-4 text-sm font-semibold text-left">
                                 Participantes
-                        </th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">
-                            Data de Inscrição
-                        </th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">
+                            </th>
+                            <th className="py-3 px-4 text-sm font-semibold text-left">
+                                Data de Inscrição
+                            </th>
+                            <th className="py-3 px-4 text-sm font-semibold text-left">
                                 Data do check-in
-                        </th>
-                        <th
-                            style={{ width: 48 }}
-                            className="py-3 px-4 text-sm font-semibold text-left"> 
-                       </th>
-                         </tr>
-        </thead>
-          <tbody>
-             {Array.from({ length: 9 }).map((_, i) => {
-                    return (
-                        <tr key={i} className="border-b border-white/10 hover: bg-white/5">
-                            
-                        <td className="py-3 px-4 text-sm text-zinc-300">
-                    <input type="checkbox" className='size-4 bg-black/20 rounded'/>
-                       </td>
-                        <td className="py-3 px-4 text-sm text-zinc-300">
+                            </th>
+                            <th
+                                style={{ width: 48 }}
+                                className="py-3 px-4 text-sm font-semibold text-left"
+                            ></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.from({ length: 9 }).map((_, i) => {
+                            return (
+                                <tr
+                                    key={i} className="border-b border-white/10 hover:bg-white/5">
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
+                                        <input
+                                            type="checkbox"
+                                            className="size-4 bg-black/20 rounded"
+                                        />
+                                    </td>
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
                                         123456
-                       </td>
-                        <td className="py-3 px-4 text-sm text-zinc-300">
-                       <div className="flex flex-col gap-1">
+                                    </td>
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
+                                        <div className="flex flex-col gap-1">
+                                            <span className="font-semibold text-white">
+                                                Diego Marcos
+                                            </span>
 
-                      <span className="font-semibold text-white">
-                                       Diego Marcos 
-                      </span>
-
-                      <span>diego@gmail.com.com</span>
-                  </div>
-                      </td>
-                        <td className="py-3 px-4 text-sm text-zinc-300">
+                                            <span>diego@gmail.com.com</span>
+                                        </div>
+                                    </td>
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
                                         7 dias atrás
-                      </td>
-                       <td className="py-3 px-4 text-sm text-zinc-300">
+                                    </td>
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
                                         3 dias atrás
-                     </td>
-                      <td className="py-3 px-4 text-sm text-zinc-300">
-                  <button className="bg-black/20 border border-white/10 rounded-md px-1.5">
-          <MoreHorizontal className="size 4" />
-                 </button>
-                    </td>
-                    </tr>
-                    )
-                })}
-                </tbody>
-                  <tfoot>
-                    <tr>
-                      <td
-                         className="py-3 px-4 text-sm text-zinc-300"
-                         colSpan={3}>
+                                    </td>
+                                    <td className="py-3 px-4 text-sm text-zinc-300">
+                                        <button className="bg-black/20 border border-white/10 rounded-md px-1.5">
+                                            <MoreHorizontal className="size 4" />
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td
+                                className="py-3 px-4 text-sm text-zinc-300"
+                                colSpan={3}
+                            >
                                 Mostrando 10 de 228 itens
-                     </td>
-                      <td
-                        className="py-3 px-4 text-sm text-zinc-300 text-right"
-                        colSpan={3}>
-                  <div className="inline-flex items-center gap-8">
-                 <span>Página 1 de 23</span>
-                  <div className="flex gap-1.5">
-               <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
-         <ChevronsLeft className="size 4" />
-              </button>
-               <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
-          <ChevronLeft className="size 4" />
-              </button>
-               <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
-         <ChevronRight className="size 4" />
-              </button>
-               <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
-        <ChevronsRight className="size 4" />
-              </button>
-                 </div>
-                    </div>
-                        </td>
-                          </tr>
+                            </td>
+                            <td
+                                className="py-3 px-4 text-sm text-zinc-300 text-right"
+                                colSpan={3}
+                            >
+                                <div className="inline-flex items-center gap-8">
+                                    <span>Página 1 de 23</span>
+                                    <div className="flex gap-1.5">
+                                        <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
+                                            <ChevronsLeft className="size 4" />
+                                        </button>
+                                        <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
+                                            <ChevronLeft className="size 4" />
+                                        </button>
+                                        <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
+                                            <ChevronRight className="size 4" />
+                                        </button>
+                                        <button className="bg-white/10 border border-white/10 rounded-md px-1.5">
+                                            <ChevronsRight className="size 4" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
                     </tfoot>
                 </table>
             </div>
